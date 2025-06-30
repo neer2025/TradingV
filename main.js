a2894901b28f6cd2f5b6e140c515ca9fdca11f81
Anmol Khurana <anmolkhurana490@gmail.com>
2025-06-30 23:20:08 +0530

    navigating page detached issue resolved

--- main.js
+++ main.js
@@ -13,9 +13,23 @@ const CONFIG = {
     }
 };
 
-const getNewsArticleData = async (browser, path) => {
+const newPage = async (browser) => {
     const page = await browser.newPage();
-    await page.goto(CONFIG.paths.pageUrl + path, { waitUntil: 'networkidle2' });
+
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36");
+
+    await page.setExtraHTTPHeaders({
+        'Accept-Language': 'en-US,en;q=0.9',
+    });
+
+    await page.setViewport({ width: 1920, height: 1080 });
+    return page;
+}
+
+const getNewsArticleData = async (browser, path) => {
+    const page = await newPage(browser);
+    await page.goto(CONFIG.paths.pageUrl + path, { waitUntil: 'domcontentloaded' });
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     const newsArticle = await page.$eval(
         'div.body-KX2tCBZq > div:nth-child(2) > span',
@@ -28,12 +42,15 @@ const getNewsArticleData = async (browser, path) => {
 
 
 const extractandSaveCompanyData = async (browser, link, symbol) => {
-    const page = await browser.newPage();
+    const page = await newPage(browser);
+
+    // await page.setDefaultNavigationTimeout(2000);
     await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 2 seconds before navigating
 
     try {
-        await page.goto(link, { waitUntil: 'networkidle2' });
-        await page.waitForSelector('div.list-iTt_Zp4a', { timeout: 2000 });
+        await page.goto(link, { waitUntil: 'domcontentloaded', timeout: 5000 });
+        await page.waitForSelector('div.list-iTt_Zp4a', { timeout: 5000, visible: true });
+        await new Promise(resolve => setTimeout(resolve, 2000));
 
         // Extract news items directly from the page
         const newsMeta = await page.$$eval('div.list-iTt_Zp4a > a.card-BpSwpmE_', nodes =>
@@ -106,16 +123,14 @@ const saveDatatoSQL = async (data) => {
 
 const run = async (jsonData, start, end) => {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--single-process',
-            '--disable-gpu',
-            '--disable-features=site-per-process',
-            '--disable-extensions',
-            '--disable-blink-features=AutomationControlled',
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--window-size=1920,1080",
+            "--disable-web-security",
+            "--disable-features=IsolateOrigins,site-per-process",
+            "--disable-dev-shm-usage",
         ]
     });
     await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 2 seconds before starting
